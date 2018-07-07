f7ac861a0b58d76bed99eee0e3304d2e2dffac84
Glendleton <gpendlet92@gmail.com>
2018-07-07 15:22:51 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// first step4)Create three objects with four properties (one must be an image file path) using object literal notation.
+
+// step5)Create the same three objects, with the same four properties, using constructor notation.
+
+// step6)Add to a method to each literal object. The method should tmultiply two numbers and return the product.
+
+// step7)Add the same method to your constructor function.
+
+// step8)Add each object created by the constructor function to an array.
+
+// step9)Loop through the array of objects. Create an element for each property so you have a place to display each piece of information.
+
+// step10)Still in the loop, create individual text nodes for each piece of information. Add these text nodes to the elements you created.
+
+// step11)Still in the loop, update the source and alt attributes of the image.
+
+// step12)in html
+
+// step13)Still in the loop, finally add the contianer to the HTML so the user can view the content.
